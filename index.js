7f9635ef9cd7b01f326657165e2a385fc3d45178
sandeep <54143606+SandeepSINGH-ENG@users.noreply.github.com>
2022-07-15 17:34:43 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+$(".slick").slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow:"<div class='pre'><i class='fa fa-angle-left' aria-hidden='true'></i></div>",
+      nextArrow:"<div class='next'><i class='fa fa-angle-right' aria-hidden='true'></i></div>"
+});
